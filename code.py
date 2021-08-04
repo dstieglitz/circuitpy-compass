15eab82d69a4364097f0c59d31d50e89921e2e5d
Dan Stieglitz <dstieglitz@stainlesscode.com>
2021-08-03 20:01:44 -0700

    Added license preamble

--- code.py
+++ code.py
@@ -1,5 +1,25 @@
+# 
+# circuitpy-compass (code.py)
+#
 # This application renders a simulated magnetic compass on an embedded display. The input is a single
 # integer indicating what heading to display in the range (0,359).
+#
+# Copyright (C) 2021 Dan Stieglitz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import usb_cdc
 import displayio
 import board
